8c6d34df64c06d2a32db92b05ea634393f633bd6
bxyzhu <arcmandak@ucla.edu>
2016-06-08 18:25:23 -0600

    Fixed cable centers

--- MJDAnalysis/LowEnergyFitter.cc
+++ MJDAnalysis/LowEnergyFitter.cc
@@ -4,5 +4,24 @@ using namespace RooFit;
 void LowEnergyFitter()
 {
 
+}
+
+void DoTheFit()
+{
+
+}
+
+void LoadDataFromFile()
+{
+
+}
+
+void GeneratePDF()
+{
+
+}
+
+void CalculateEfficiency()
+{
 
 }
