6e97035f702936fc6be1fec5fd28cb9d427edcb9
bxyzhu <arcmandak@ucla.edu>
2016-06-17 13:06:00 -0600

    Fixed XY positioning of cables as well as rotation (for only one cryostat). Need to fix Z positioning

--- MJDGeant4/MGGeneratorMJDCable.cc
+++ MJDGeant4/MGGeneratorMJDCable.cc
@@ -85,7 +85,163 @@ MGGeneratorMJDCable::MGGeneratorMJDCable()
   fG4Messenger = new MGGeneratorMJDCableMessenger(this);
   fParticleGun = new G4ParticleGun(1);
 
+  std::string sourcePos = "W";
+  G4UIcommandTree* cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
+  // cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
+  cmdTree = cmdTree->GetTree(G4String("/MG/demonstrator/"));
+  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
+  {
+    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
+    if(param == "cryo1Pos" || param == "cryo2Pos")
+    {
+      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
+      G4double x, y, z;
+      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
+      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
+      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
+      if(param == "cryo1Pos" && sourcePos == "W")
+  fColdPlateOffset[0] = G4ThreeVector(x, y, z);
+      else if(param == "cryo2Pos" && sourcePos == "E")
+  fColdPlateOffset[0] = G4ThreeVector(x, y, z);
+    }
+    else if(param == "cryo1Rot" && sourcePos == "W")
+    {
+      std::string val =
+  cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
+      // std::stringstream(val) >> fZrotation;
+    }
+    else if(param == "cryo2Rot" && sourcePos == "E")
+    {
+      std::string val =
+  cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
+      // std::stringstream(val) >> fZrotation;
+    }
+  }
+  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
+  cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocryoassembly"+sourcePos+"/"));
+  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
+  {
+    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
+    if(param == "calAssemblyPos"){
+      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
+      G4double x, y, z;
+      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
+      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
+      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
+      fColdPlateOffset[0] += G4ThreeVector(x, y, z/2); // use 0 here
+    }
+  }
+  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
+  cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocalassembly"+sourcePos+"/"));
+  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
+  {
+    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
+    if(param == "sourceOffset")
+    {
+      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
+      G4double x, y, z;
+      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
+      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
+      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
+      fColdPlateOffset[0] += G4ThreeVector(x, y, z);
+    }
+  }
+  // fZrotation *= -1;
+
+  // fColdPlateOffset[0].rotateZ(pi/2);
+
+  fCableRadius = 0.5*mm;
+
+  // Offsets are listed with signal before HV (signal is left of string)
+  // Units are converted to cm
+  fCableOffset[0] = G4ThreeVector(-2.54*1.839*cm, 2.54*0.560*cm, 0.); // P1
+  fCableOffset[1] = G4ThreeVector(-2.54*1.839*cm, -2.54*0.560*cm, 0.);  
+  fCableOffset[2] = G4ThreeVector(2.54*3.453*cm, 2.54*3.846*cm, 0.); // P2
+  fCableOffset[3] = G4ThreeVector(2.54*2.350*cm, 2.54*3.620*cm, 0.);
+  fCableOffset[4] = G4ThreeVector(-2.54*0.089*cm, 2.54*5.576*cm, 0.); // P3
+  fCableOffset[5] = G4ThreeVector(-2.54*1.186*cm, 2.54*5.350*cm, 0.);
+  fCableOffset[6] = G4ThreeVector(-2.54*3.356*cm, 2.54*3.846*cm, 0.); // P4
+  fCableOffset[7] = G4ThreeVector(-2.54*4.387*cm, 2.54*3.408*cm, 0.);
+  fCableOffset[8] = G4ThreeVector(-2.54*4.387*cm, -2.54*3.408*cm, 0.); // P5
+  fCableOffset[9] = G4ThreeVector(-2.54*3.356*cm, -2.54*3.846*cm, 0.);
+  fCableOffset[10] = G4ThreeVector(-2.54*1.186*cm, -2.54*5.350*cm, 0.); // P6
+  fCableOffset[11] = G4ThreeVector(-2.54*0.089*cm, -2.54*5.576*cm, 0.);
+  fCableOffset[12] = G4ThreeVector(2.54*2.350*cm, -2.54*3.620*cm, 0.); // P7
+  fCableOffset[13] = G4ThreeVector(2.54*3.453*cm, -2.54*3.846*cm, 0.);
+
+  if(sourcePos == "W")
+  {
+    for(int i = 0; i < 14; i++)
+    {
+      // Rotate for cryo 1
+      fCableOffset[i].rotateZ(pi/2);
+    }
+  }
+
+}
+
+//---------------------------------------------------------------------------//
+
+MGGeneratorMJDCable::MGGeneratorMJDCable(const MGGeneratorMJDCable & other) : MGVGenerator(other)
+{;}
+
+//---------------------------------------------------------------------------//
+
+MGGeneratorMJDCable::~MGGeneratorMJDCable()
+{
+  delete fG4Messenger;
+  delete fParticleGun;
+}
+
+//---------------------------------------------------------------------------//
+
+void MGGeneratorMJDCable::BeginOfRunAction(G4Run const*)
+{;}  
+
+//---------------------------------------------------------------------------//
+
+void MGGeneratorMJDCable::EndOfRunAction(G4Run const*)
+{;}
+
+//---------------------------------------------------------------------------//
+
+void MGGeneratorMJDCable::GeneratePrimaryVertex(G4Event *event)
+{
+
+  // Generate random variables
+  fRandString = G4RandFlat::shootInt(14);
+  fRandPos = G4RandFlat::shootInt(4);
+  fRandRadiusSq = fCableRadius*fCableRadius*G4UniformRand();
+  fRandAngle = 2*pi*G4UniformRand();
+  
+  // Choose a random XY point along a disk
+  fPositionX = sqrt( fRandRadiusSq ) * cos( fRandAngle );
+  fPositionY = sqrt( fRandRadiusSq ) * sin( fRandAngle );
+  // Choose a random cable and generate a random point along the cable
+  fPositionZ = (1. - 2.*G4UniformRand())*fCableLength[fRandPos];
+
+  // Set source position
+  fPosition = fColdPlateOffset[0] + fCableOffset[fRandString] + G4ThreeVector(fPositionX, fPositionY, fPositionZ + fCableCenter[fRandPos]);
+
+  fParticleGun->SetParticleDefinition(G4Gamma::GammaDefinition()); // Gamma for testing
+  fParticleGun->SetParticleEnergy(1500.0*MeV); 
+
+  // G4IonTable *theIonTable = (G4IonTable*)(G4ParticleTable::GetParticleTable()->GetIonTable());
+  // G4ParticleDefinition *aIon = theIonTable->GetIon(fZ, fA);
+
+  // fParticleGun->SetParticleDefinition(aIon);
+  // fParticleGun->SetParticleEnergy(0.0);
+  fParticleGun->SetParticleMomentumDirection(G4RandomDirection());
+  fParticleGun->SetParticlePosition(fPosition);
+  fParticleGun->GeneratePrimaryVertex(event);
+}
+
+//---------------------------------------------------------------------------//
+
 /*
+void MGGeneratorMJDCable::SetSourcePos()
+{
+  // Doesn't work
   // Cryostat global position, from MJGeometryDemonstrator
   G4ThreeVector fCryo1Pos = G4ThreeVector(-8.1417 * 25.4 * mm, 0.0, 4.4265 * 25.4 * mm);
   G4double fCryo1Rot = pi / 2;
@@ -132,239 +288,4 @@ MGGeneratorMJDCable::MGGeneratorMJDCable()
   fColdPlateOffset[0] *= *CPglobalRot_final1;
 */
 
-  std::string sourcePos = "W";
-  G4UIcommandTree* cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  // cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  cmdTree = cmdTree->GetTree(G4String("/MG/demonstrator/"));
-  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  {
-    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
-    if(param == "cryo1Pos" || param == "cryo2Pos")
-    {
-      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
-      G4double x, y, z;
-      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
-      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
-      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
-      if(param == "cryo1Pos" && sourcePos == "W")
-  fColdPlateOffset[0] = G4ThreeVector(x, y, z);
-      else if(param == "cryo2Pos" && sourcePos == "E")
-  fColdPlateOffset[0] = G4ThreeVector(x, y, z);
-    }
-    else if(param == "cryo1Rot" && sourcePos == "W")
-    {
-      std::string val =
-  cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
-      // std::stringstream(val) >> fZrotation;
-    }
-    else if(param == "cryo2Rot" && sourcePos == "E")
-    {
-      std::string val =
-  cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
-      // std::stringstream(val) >> fZrotation;
-    }
-  }
-  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocryoassembly"+sourcePos+"/"));
-  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  {
-    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
-    if(param == "calAssemblyPos"){
-      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
-      G4double x, y, z;
-      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
-      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
-      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
-      fColdPlateOffset[0] += G4ThreeVector(x, y, z);
-    }
-  }
-  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocalassembly"+sourcePos+"/"));
-  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  {
-    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
-    if(param == "sourceOffset")
-    {
-      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
-      G4double x, y, z;
-      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
-      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
-      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
-      fColdPlateOffset[0] += G4ThreeVector(x, y, z);
-    }
-  }
-  // fZrotation *= -1;
 
-  // fColdPlateOffset[0].rotateZ(pi/2);
-
-  fCableRadius = 0.5*mm;
-
-  // fCableCenter[4] = {0.,0.,0.,0.};
-  // fCableLength[4] = {2.54*10.0/2*cm, 2.54*8.0/2*cm, 2.54*5.0/2*cm, 2.54*2.5/2*cm};
-
-  // Offsets are listed with signal before HV (signal is left of string)
-  // Units are converted to cm
-  fCableOffset[0] = G4ThreeVector(-2.54*1.839*cm, 2.54*0.560*cm, 0.); // P1
-  fCableOffset[1] = G4ThreeVector(-2.54*1.839*cm, -2.54*0.560*cm, 0.);  
-  fCableOffset[2] = G4ThreeVector(2.54*3.453*cm, 2.54*3.846*cm, 0.); // P2
-  fCableOffset[3] = G4ThreeVector(2.54*2.350*cm, 2.54*3.620*cm, 0.);
-  fCableOffset[4] = G4ThreeVector(-2.54*0.089*cm, 2.54*5.576*cm, 0.); // P3
-  fCableOffset[5] = G4ThreeVector(-2.54*1.186*cm, 2.54*5.350*cm, 0.);
-  fCableOffset[6] = G4ThreeVector(-2.54*3.356*cm, 2.54*3.846*cm, 0.); // P4
-  fCableOffset[7] = G4ThreeVector(-2.54*4.387*cm, 2.54*3.408*cm, 0.);
-  fCableOffset[8] = G4ThreeVector(-2.54*4.387*cm, -2.54*3.408*cm, 0.); // P5
-  fCableOffset[9] = G4ThreeVector(-2.54*3.356*cm, -2.54*3.846*cm, 0.);
-  fCableOffset[10] = G4ThreeVector(-2.54*1.186*cm, -2.54*5.350*cm, 0.); // P6
-  fCableOffset[11] = G4ThreeVector(-2.54*0.089*cm, -2.54*5.576*cm, 0.);
-  fCableOffset[12] = G4ThreeVector(2.54*2.350*cm, -2.54*3.620*cm, 0.); // P7
-  fCableOffset[13] = G4ThreeVector(2.54*3.453*cm, -2.54*3.846*cm, 0.);
-
-}
-
-//---------------------------------------------------------------------------//
-
-MGGeneratorMJDCable::MGGeneratorMJDCable(const MGGeneratorMJDCable & other) : MGVGenerator(other)
-{;}
-
-//---------------------------------------------------------------------------//
-
-MGGeneratorMJDCable::~MGGeneratorMJDCable()
-{
-  delete fG4Messenger;
-  delete fParticleGun;
-}
-
-//---------------------------------------------------------------------------//
-
-void MGGeneratorMJDCable::BeginOfRunAction(G4Run const*)
-{;}  
-
-//---------------------------------------------------------------------------//
-
-void MGGeneratorMJDCable::EndOfRunAction(G4Run const*)
-{;}
-
-//---------------------------------------------------------------------------//
-
-void MGGeneratorMJDCable::GeneratePrimaryVertex(G4Event *event)
-{
-
-  // Generate random variables
-  fRandString = G4RandFlat::shootInt(14);
-  fRandPos = G4RandFlat::shootInt(4);
-  fRandRadiusSq = fCableRadius*fCableRadius*G4UniformRand();
-  fRandAngle = 2*pi*G4UniformRand();
-  
-  // Choose a random XY point along a disk
-  fPositionX = sqrt( fRandRadiusSq ) * cos( fRandAngle );
-  fPositionY = sqrt( fRandRadiusSq ) * sin( fRandAngle );
-  // Choose a random cable and generate a random point along the cable
-  fPositionZ = (1. - 2.*G4UniformRand())*fCableLength[fRandPos];
-
-  // Set source position
-  fPosition = fColdPlateOffset[0] + fCableOffset[fRandString].rotateZ(pi/2) + G4ThreeVector(fPositionX, fPositionY, fPositionZ + fCableCenter[fRandPos]);
-
-  // G4IonTable *theIonTable = (G4IonTable*)(G4ParticleTable::GetParticleTable()->GetIonTable());
-  // G4ParticleDefinition *aIon = theIonTable->GetIon(fZ, fA);
-
-  // fParticleGun->SetParticleDefinition(aIon);
-  // fParticleGun->SetParticleEnergy(0.0);
-  fParticleGun->SetParticleMomentumDirection(G4RandomDirection()); // Geantinos for testing
-  // fParticleGun->SetParticleDefinition(G4Geantino::Geantino());
-  fParticleGun->SetParticleDefinition(G4Gamma::GammaDefinition());
-  fParticleGun->SetParticleEnergy(2000.0*MeV); 
-  fParticleGun->SetParticlePosition(fPosition);
-  fParticleGun->GeneratePrimaryVertex(event);
-}
-
-//---------------------------------------------------------------------------//
-
-/*
-void MGGeneratorMJDCable::SetSourcePos()
-{
-
-  std::string sourcePos = "W";
-  
-  G4UIcommandTree* cmdTree =
-    G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  // cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocalsource"+sourcePos+"/"));
-  // for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  // {
-  //   if(cmdTree->GetCommand(i+1)->GetParameterEntries() == 0)
-  //     continue;
-  //   std::string val =
-  //     cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
-  //   std::string param =
-  //     cmdTree->GetCommand(i+1)->GetCommandName();
-  //   if(param == "helixRadius")
-  //     std::stringstream(val) >> fHelixRadius;
-  //   else if(param == "startAngle")
-  //     std::stringstream(val) >> fStartAngle;
-  //   else if(param == "totalAngle")
-  //     std::stringstream(val) >> fTotalAngle;
-  //   else if(param == "helixAngle")
-  //     std::stringstream(val) >> fHelixAngle;
-  // }
-
-  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  cmdTree = cmdTree->GetTree(G4String("/MG/demonstrator/"));
-  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  {
-    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
-    if(param == "cryo1Pos" || param == "cryo2Pos")
-    {
-      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
-      G4double x, y, z;
-      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
-      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
-      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
-      if(param == "cryo1Pos" && sourcePos == "W")
-  fColdPlateOffset[0] = G4ThreeVector(x, y, z);
-      else if(param == "cryo2Pos" && sourcePos == "E")
-  fColdPlateOffset[0] = G4ThreeVector(x, y, z);
-    }
-    else if(param == "cryo1Rot" && sourcePos == "W")
-    {
-      std::string val =
-  cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
-      std::stringstream(val) >> fZrotation;
-    }
-    else if(param == "cryo2Rot" && sourcePos == "E")
-    {
-      std::string val =
-  cmdTree->GetCommand(i+1)->GetParameter(0)->GetDefaultValue();
-      std::stringstream(val) >> fZrotation;
-    }
-  }
-  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocryoassembly"+sourcePos+"/"));
-  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  {
-    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
-    if(param == "calAssemblyPos"){
-      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
-      G4double x, y, z;
-      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
-      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
-      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
-      fColdPlateOffset[0] += G4ThreeVector(x, y, z);
-    }
-  }
-  cmdTree = G4UImanager::GetUIpointer()->GetTree()->GetTree("/MG/");
-  cmdTree = cmdTree->GetTree(G4String("/MG/mjdemocalassembly"+sourcePos+"/"));
-  for(int i=0; i<cmdTree->GetCommandEntry(); i++)
-  {
-    std::string param = cmdTree->GetCommand(i+1)->GetCommandName();
-    if(param == "sourceOffset")
-    {
-      G4UIcommand* cmd = cmdTree->GetCommand(i+1);
-      G4double x, y, z;
-      std::stringstream(cmd->GetParameter(0)->GetDefaultValue()) >> x;
-      std::stringstream(cmd->GetParameter(1)->GetDefaultValue()) >> y;
-      std::stringstream(cmd->GetParameter(2)->GetDefaultValue()) >> z;
-      fColdPlateOffset[0] += G4ThreeVector(x, y, z);
-    }
-  }
-  fZrotation *= -1;
-}
-*/
