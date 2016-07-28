5d37a75bc3f30542221a74b3dfa3c6f68d4c8cf6
bxyzhu <arcmandak@ucla.edu>
2016-07-28 11:59:41 -0600

    Fixed CF Start time finder

--- MJDAnalysis/MGWFCFStartTime.cc
+++ MJDAnalysis/MGWFCFStartTime.cc
@@ -3,26 +3,28 @@
 using namespace std;
 
 MGWFCFStartTime::MGWFCFStartTime(const string& aName) :
-  MGVWaveformParameterCalculator(aName), fRatio(0.5), fOffset(150)
+  MGVWaveformParameterCalculator(aName), fRatio(0.5), fOffset(1.5*us)
 {
   AddParameter("CFt0");
 }
 
 void MGWFCFStartTime::CalculateParameters(const MGWaveform& anInput)
 {
-  size_t offSet = static_cast<size_t>(fOffset*anInput.GetSamplingFrequency());
+  // Convert delay from units of time to sample
+  size_t iOffset = static_cast<size_t>(fOffset*anInput.GetSamplingFrequency());
 
-  // Create the delayed + inverted pulse and add it to the main waveform
-  // Scale down by some fraction and invert, start with the fOffset'th sample (shifts scaled pulse backwards in time)
-  // Vector length is reduced by fOffset samples
-  for(size_t i = offSet; i < anInput.GetLength(); i++) fScaledInput.push_back(-fRatio*anInput[i]);
+  // Create the delayed + inverted pulse
+  // Scale down by some fraction and invert, start with the iOffset'th sample
+  for(size_t i = iOffset; i < anInput.GetLength(); i++) fScaledInput.push_back(-fRatio*anInput[i]);
 
-  // Sum together the inverted and delayed waveform with the original
-  for(size_t i = 0; i < anInput.GetLength()-offSet; i++) fSummedVector.push_back( anInput[i] + fScaledInput[i] );
+  // Sum together the inverted and delayed waveform with the original, vector length is reduced by iOffset samples
+  for(size_t i = 0; i < anInput.GetLength()-iOffset; i++) fSummedVector.push_back( anInput[i] + fScaledInput[i] );
   
   // Start at offset sample and then walk foward until reaching 0 crossing
-  size_t iRef = offSet;
-  while(anInput[iRef] < 0.) iRef++;
-  SetParameterValue(0, anInput.InterpolateForTLocal(0., iRef-1, iRef));
+  size_t iRef = iOffset;
+  while(fSummedVector[iRef] < 0.) iRef++;
 
+  // Interpolate with previous sample for t0
+  SetParameterValue(0, anInput.InterpolateForTLocal(0., iRef-1, iRef));
+  // SetParameterValue(0, iRef/anInput.GetSamplingFrequency());
 }
--- MJDAnalysis/MGWFCFStartTime.hh
+++ MJDAnalysis/MGWFCFStartTime.hh
@@ -25,7 +25,16 @@
  *
  * DESCRIPTION: 
  *
- * Estimates the start time (t0) of a trapezoidal filter using Constant Fraction method
+ * Simple start time (t0) estimator using constant fraction (CF) timing method:
+ * The premise of CF timing is to always evaluate the t0 at a fixed time
+ * after the leading edge of the pulse has reached a constant fraction of the peak pulse amplitude
+ *
+ * The original signal is first inverted and multiplied by a fraction (representing the evaluation point)
+ * The original signal is also delayed (typically by a time larger than the rise-time) and the two signals
+ * are summed together; the zero point crossing represents the time at which the pulse reaches the fraction. 
+ *
+ * As the rise-time and shape of the waveform can vary greatly, trigger walk is reduced by 
+ * reducing the delay time (also known as amplitude and rise-time compensated timing)
  *
  * AUTHOR: B. Zhu
  * FIRST SUBMISSION: 
@@ -53,10 +62,10 @@ class MGWFCFStartTime : public MGVWaveformParameterCalculator
     virtual void CalculateParameters(const MGWaveform& inputWF);
 
   protected:
-    double fRatio; // Ratio to evaluate fraction
-    double fOffset; // Time delay in units of samples
-    std::vector<double> fScaledInput; // Internal vector for inversion
-    std::vector<double> fSummedVector; // Internal vector for summing
+    double fRatio; // Ratio of the delayed waveform
+    double fOffset; // Time delay
+    std::vector<double> fScaledInput; // Internal vector for inverted + scaled waveform
+    std::vector<double> fSummedVector; // Internal vector for summing and evaluating t0
 
     MGWaveformRegion fRegion;
     MGWaveform fWF;
