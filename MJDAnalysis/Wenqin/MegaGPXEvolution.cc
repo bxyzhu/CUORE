e8ae017de51172e61128024f078e7ca5c7b47260
bxyzhu <arcmandak@ucla.edu>
2017-12-15 10:32:04 -0700

    Changed exposure

--- MJDAnalysis/Wenqin/MegaGPXEvolution.cc
+++ MJDAnalysis/Wenqin/MegaGPXEvolution.cc
@@ -35,36 +35,23 @@ int main(int argc, char** argv)
   	float fitMin = atof(argv[2]);
 	float fitMax = atof(argv[3]);
 	string ftype = argv[4];
-	// int fidx = atoi(argv[5]);
-  	//// For drawing pretty shit
 	gStyle->SetOptStat(0);
 	gStyle->SetPalette(kRainBow);
 
-	std::map<int, std::vector<double>> liveTime;
-	// liveTime[0] = {449.241, 185.208};
-	// liveTime[1] = {661.597, 66.0657};
-	// liveTime[3] = {331.405, 66.4533};
-	// liveTime[4] = {105.232, 93.5911};
-	liveTime[0] = {332.269, 64.8862};
-	liveTime[1] = {646.728, 65.6867};
-	liveTime[3] = {327.951, 44.8998};
-	liveTime[4] = {89.6705, 67.3392};
-	liveTime[5] = {662.177+319.476, 154.725+189.524};
-	int bNat = 0;
+  // Prelim exposure
+  std::map<int, std::vector<double>> liveTime;
+	liveTime[0] = {460.054, 171.021};
+	liveTime[1] = {661.811, 63.2937};
+  liveTime[2] = {106.286, 10.6791};
+	liveTime[3] = {368.52, 81.7408};
+	liveTime[4] = {102.858, 73.8446};
+	liveTime[5] = {492.158+182.193, 138.461+197.769};
+  liveTime[6] = {460.054+661.811+106.286+368.52+102.858+492.158+182.193, 171.021+63.2937+10.6791+81.7408+73.8446+138.461+197.769};
+  int bNat = 0;
 
 	//// Set cuts here
-  // if dsNum == 0 and ch == 656: continue
-  // if dsNum == 1 and ch == 578: continue
-  // if dsNum == 3 and ch == 592: continue
-  // if dsNum == 5 and ch == 1172: continue
     string theCut = "";
   	theCut += Form("trapENFCal>=%.2f&&trapENFCal<=%.2f", fitMin, fitMax); // Energy cut for fit range
-  	if(fDS == 0) theCut += "&&channel!=656";
-    else if(fDS == 1) theCut += "&&channel!=578";
-  	else if(fDS == 3) theCut += "&&channel!=592";
-  	// else if(fDS == 4) theCut += "&&!(run==60001692 && (channel==1144))&&channel!=1332";
-  	else if(fDS == 5) theCut += "&&channel!=1172";
-  	else if(fDS == 6) theCut += "&&!(run==6811&&(channel==600||channel==696)) && !(run==60001692 && (channel==1144))";
 
   	if(ftype == "Nat" || ftype == "nat"){
   		theCut += "&&isNat"; // Set Enriched or Natural
@@ -86,18 +73,17 @@ int main(int argc, char** argv)
 	if(ftype == "Nat" || ftype == "nat" || ftype == "Enr" || ftype == "enr") ValMap = RunWenqin(argList, fDS, fitMin, fitMax, ftype, theCut);
 	// else  ValMap = RunWenqin(argList, fDS, fitMin, fitMax, theCut);
 	cout << "LiveTime: " << liveTime[fDS][bNat] << endl;
-	for(auto &kv : ValMap)
+
+  // Calculate Rates
+  for(auto &kv : ValMap)
 	{
 		if(kv.first == "Tritium") {
 			cout << kv.first << ":[" << kv.second[0]*tritScale << "," << kv.second[1]*tritScale << "," << kv.second[2]*tritScale << "]"<< endl;
-			cout << "Rates (c/kg/day):" << kv.second[0]*tritScale/liveTime[fDS][bNat] << " +" << kv.second[1]*tritScale/liveTime[fDS][bNat] << " " << kv.second[2]*tritScale/liveTime[fDS][bNat] << endl;
-			cout << "Rates (2-4 keV) (c/kg/day):" << kv.second[0]*tritScale*0.224487/liveTime[fDS][bNat] << " +" << kv.second[1]*tritScale*0.224487/liveTime[fDS][bNat] << " " << kv.second[2]*tritScale*0.224487/liveTime[fDS][bNat] << endl;
 			cout << "Rates (c/keV/kg/day):" << kv.second[0]*tritScale/liveTime[fDS][bNat]/(fitMax-fitMin) << " +" << kv.second[1]*tritScale/liveTime[fDS][bNat]/(fitMax-fitMin) << " " << kv.second[2]*tritScale/liveTime[fDS][bNat]/(fitMax-fitMin) << endl;
 			cout << "Rates (2-4 keV) (c/keV/kg/day):" << kv.second[0]*tritScale*0.224487/liveTime[fDS][bNat]/(fitMax-fitMin) << " +" << kv.second[1]*tritScale*0.224487/liveTime[fDS][bNat]/(fitMax-fitMin) << " " << kv.second[2]*tritScale*0.224487/liveTime[fDS][bNat]/(fitMax-fitMin) << endl;
 		}
 		else {
 			cout << kv.first << ":[" << kv.second[0] << "," << kv.second[1] << "," << kv.second[2] << "]"<< endl;
-			cout << "Rates (c/kg/day):" << kv.second[0]/liveTime[fDS][bNat] << " +" << kv.second[1]/liveTime[fDS][bNat] << " " << kv.second[2]/liveTime[fDS][bNat] << endl;
 			cout << "Rates (c/keV/kg/day):" << kv.second[0]/liveTime[fDS][bNat]/(fitMax-fitMin) << " +" << kv.second[1]/liveTime[fDS][bNat]/(fitMax-fitMin) << " " << kv.second[2]/liveTime[fDS][bNat]/(fitMax-fitMin) << endl;
 		}
 	}
@@ -121,14 +107,20 @@ map<string, vector<double>> RunWenqin(vector<string> argS, int fDS, float fitMin
 
 	// Load data from TChain with a cut string
 	TChain *skimTree = new TChain("skimTree");
-    if(fDS == 6) skimTree->Add("/Users/brianzhu/project/cuts/hadd/Reduced_*.root" );
-  	else skimTree->Add(Form("/Users/brianzhu/project/cuts/hadd/Reduced_DS%d_*.root", fDS) );
+    if(fDS == 6) {
+      skimTree->Add("/Users/brianzhu/project/cuts/corrfs_rn/corrfs_rn-DS1-*.root" );
+      skimTree->Add("/Users/brianzhu/project/cuts/corrfs_rn/corrfs_rn-DS2-*.root" );
+      skimTree->Add("/Users/brianzhu/project/cuts/corrfs_rn/corrfs_rn-DS3-*.root" );
+      skimTree->Add("/Users/brianzhu/project/cuts/corrfs_rn/corrfs_rn-DS4-*.root" );
+      skimTree->Add("/Users/brianzhu/project/cuts/corrfs_rn/corrfs_rn-DS5-*.root" );
+    }
+  	else skimTree->Add(Form("/Users/brianzhu/project/cuts/corrfs_rn/corrfs_rn-DS%d-*.root", fDS) );
   	fitter->LoadChainData(skimTree, theCut);
 
 	// Construct PDF and do fit
 	fitter->ConstructPDF();
   fitter->DoFit();
-	fitter->DrawBasicShit(0.4, false, false);
+	fitter->DrawBasicShit(0.2, false, false);
 	fitter->GetFitResult()->Print("v");
 	map<string, vector<double>> WenqinMap;
 	for(auto &argN: argS){
